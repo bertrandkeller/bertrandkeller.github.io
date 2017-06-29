15dcca4d9d2fa4f4b80d59b010fc43eeddf58a7e
bk <bertrand.keller@gmail.com>
2017-06-29 15:21:02 +0200

    add service worker

---
+++ sw.js
@@ -0,0 +1,53 @@
+---
+layout: null
+---
+
+'use strict';
+const staticCacheName = 'version-{{site.time | date: '%Y%m%d%H%M%S'}}::';
+
+console.log("installing service worker");
+
+const filesToCache = [
+  "/",
+  {% for page in site.html_pages %}
+    '{{ page.url }}',
+  {% endfor %}
+  {% for post in site.posts %}
+    '{{ post.url }}',
+  {% endfor %}
+
+  // can be automated rather than manual entries
+  "/index.html"
+];
+
+self.addEventListener("install", function(e){
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(staticCacheName).then(function(cache){
+      return cache.addAll(filesToCache);
+    })
+  )
+});
+
+self.addEventListener("activate", function(e){
+  e.waitUntil(
+    caches.keys().then(function(cacheNames){
+      return Promise.all(
+        cacheNames.filter(function(cacheName){
+          return cacheName.startsWith("version-")
+            && cacheName != staticCacheName;
+        }).map(function(cacheName){
+          return cache.delete(cacheName);
+        })
+      )
+    })
+  )
+});
+
+self.addEventListener("fetch", function(e){
+  e.respondWith(
+     caches.match(e.request).then(function(response) {
+       return response || fetch(e.request);
+     })
+   )
+});
