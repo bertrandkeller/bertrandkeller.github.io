4a2a5fc167e4342c164a8798a2267bc2a45df5b5
bk <bertrand.keller@gmail.com>
2017-07-29 11:32:57 +0200

    change srvice worker

--- sw.js
+++ sw.js
@@ -4,44 +4,89 @@ layout: null
 
 'use strict';
 const staticCacheName = 'version-{{site.time | date: '%Y%m%d%H%M%S'}}::';
+const pagesCacheName = 'pages';
+const imagesCacheName = 'images';
 
-console.log("installing service worker");
-
-const filesToCache = [
-  "/",
-  {% for page in site.html_pages %}
-    '{{ page.url }}',
-  {% endfor %}
-  {% for post in site.posts %}
-    '{{ post.url }}',
-  {% endfor %}
-
-  // can be automated rather than manual entries
-  //"/index.html"
+const cacheList = [
+    staticCacheName,
+    pagesCacheName,
+    imagesCacheName
 ];
 
-self.addEventListener("install", function(e){
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(staticCacheName).then(function(cache){
-      return cache.addAll(filesToCache);
-    })
-  )
+
+const offlinePages = [
+    "/",
+    {% for page in site.html_pages %}
+      '{{ page.url }}',
+    {% endfor %}
+    {% for post in site.posts %}
+      '{{ post.url }}',
+    {% endfor %}
+    '/index.html'
+];
+
+
+function updateStaticCache() {
+    return caches.open(staticCacheName)
+        .then( cache => {
+            // These items won't block the installation of the Service Worker
+            cache.addAll([
+                
+            ].concat(offlinePages));
+            // These items must be cached for the Service Worker to complete installation
+            return cache.addAll([
+                
+            ]);
+        });
+}
+
+function stashInCache(cacheName, request, response) {
+    caches.open(cacheName)
+        .then( cache => cache.put(request, response) );
+}
+
+// Limit the number of items in a specified cache.
+function trimCache(cacheName, maxItems) {
+    caches.open(cacheName)
+        .then( cache => {
+            cache.keys()
+                .then(keys => {
+                    if (keys.length > maxItems) {
+                        cache.delete(keys[0])
+                            .then(trimCache(cacheName, maxItems));
+                    }
+                });
+        });
+}
+
+// Remove caches whose name is no longer valid
+function clearOldCaches() {
+    return caches.keys()
+        .then( keys => {
+            return Promise.all(keys
+                .filter(key => !cacheList.includes(key))
+                .map(key => caches.delete(key))
+            );
+        });
+}
+
+self.addEventListener('install', event => {
+    event.waitUntil(updateStaticCache()
+        .then( () => self.skipWaiting() )
+    );
 });
 
-self.addEventListener("activate", function(e){
-  e.waitUntil(
-    caches.keys().then(function(cacheNames){
-      return Promise.all(
-        cacheNames.filter(function(cacheName){
-          return cacheName.startsWith("version-")
-            && cacheName != staticCacheName;
-        }).map(function(cacheName){
-          return cache.delete(cacheName);
-        })
-      )
-    })
-  )
+self.addEventListener('activate', event => {
+    event.waitUntil(clearOldCaches()
+        .then( () => self.clients.claim() )
+    );
+});
+
+self.addEventListener('message', event => {
+    if (event.data.command == 'trimCaches') {
+        trimCache(pagesCacheName, 35);
+        trimCache(imagesCacheName, 20);
+    }
 });
 
 self.addEventListener("fetch", function(e){
